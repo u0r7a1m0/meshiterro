7aa71b042db1e4aba6ffcd77c72d7e9597767f9d
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-15 12:52:01 +0000

    [Update]4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,5 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
+  # 追記
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'home/top'
   devise_for :users
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   root to:'homes#top'
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about' => 'homes#about', as: 'about'
 end
