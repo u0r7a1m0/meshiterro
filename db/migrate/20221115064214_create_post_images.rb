92ea9689db57190475181167a315a54042c8e78d
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-15 07:19:17 +0000

    [Update]4-8

---
+++ db/migrate/20221115064214_create_post_images.rb
@@ -0,0 +1,13 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+      # 下記4つ
+      t.integer :id
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
+      
+      t.timestamps
+    end
+  end
+end
