8f50295b3e9167cf366642cd0dcabc2e850e8cac
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-16 12:15:42 +0000

    [Update]4-10

--- db/migrate/20221115064214_create_post_images.rb
+++ db/migrate/20221115064214_create_post_images.rb
@@ -1,8 +1,7 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-      # 下記4つ
-      t.integer:id
+      # 下記3つ(idは自動で作られるので定義なし)
       t.string:shop_name
       t.text:caption
       t.integer:user_id
